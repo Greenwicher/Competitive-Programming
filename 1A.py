20fefb5570a49fce5c1dd83e3757349c6db676fc
Greenwicher <weizhiliu2009@gmail.com>
2015-01-03 20:17:29 +0800

    4A

--- 1A.py
+++ 1A.py
@@ -6,12 +6,17 @@ Created on Wed Jul 24 18:27:44 2013
 http://codeforces.com/problemset/problem/1/A
 
 """
+
 import math
 
-[m, n, a] = raw_input().split(' ')
-m = int(m)
-n = int(n)
-a = float(a)
+# version 1
+#[m, n, a] = raw_input().split(' ')
+#m = int(m)
+#n = int(n)
+#a = float(a)
+#
+#print int(math.ceil(m/a)*math.ceil(n/a))
 
-print int(math.ceil(m/a)*math.ceil(n/a))
-
+# version 2
+m,n,a = map(int, raw_input().split())
+print (-m/a)*(-n/a)
--- 4A.py
+++ 4A.py
@@ -6,9 +6,15 @@ Created on Wed Jul 24 18:27:44 2013
 http://codeforces.com/problemset/problem/4/A
 
 """
+
+# version 1
 input = int(raw_input())
 
 if(input % 2==0 and input != 2):
     print 'YES'
 else:
     print 'NO'
+
+# version 2
+w = int(raw_input())
+print ['YES', 'NO'][1 - int(w%2==0 and w!=2)]
