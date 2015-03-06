57620fcae813985185f903cc96c9ff55e9309ab0
Greenwicher <weizhiliu2009@gmail.com>
2015-03-06 17:49:41 +0800

    happy coding

--- 448B.py
+++ 448B.py
@@ -6,11 +6,19 @@ Created on Fri Mar  6 17:19:04 2015
 """
 
 ## version 1
-s,t=raw_input(),raw_input();k=-1
+s,t=raw_input(),raw_input()
 f=lambda x:''.join(sorted(x))
-for i in t:
-    if s[k+1:].find(i)!=-1:
-        k=s[k+1:].find(i)+k+1
-    else:
-        k=-1;break
-print [[['need tree','both'][f(t) in f(s)],'array'][f(t)==f(s)],'automaton'][k!=-1]
+def g(s,t):
+    k=-1
+    for i in t:
+        if s[k+1:].find(i)!=-1:
+            k=s[k+1:].find(i)+k+1
+        else:
+            k=-1;break
+    return k
+print [[['need tree','both'][g(f(s),f(t))!=-1],'array'][f(t)==f(s)],'automaton'][g(s,t)!=-1]
+
+## version 2
+s,t=raw_input(),raw_input()
+f,ss,tt=lambda s,t:not reduce(lambda x,y:x[x[0]==y:] if x else x, s, t),sorted(s),sorted(t)
+print 'array' if ss==tt else 'automaton' if f(s,t)  else 'both' if f(ss,tt) else 'need tree'
