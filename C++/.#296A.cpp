50de4e97958e34c1232ec5e927b78e733a8f2780
Greenwicher <weizhiliu2009@gmail.com>
2015-04-12 16:22:54 +0800

    happy coding

--- C++/.#296A.cpp
+++
@@ -1,1 +0,0 @@
-liuweizhi@LiuWeizhideMacBook-Pro.local.2176
--- C++/446A.cpp
+++ C++/446A.cpp
@@ -58,9 +58,9 @@ int main()
 }
 */
 
-
+/*
 // version 2
-// dynamic programming
+// dynamic programming (not right)
 #include<iostream>
 using namespace std;
 
@@ -103,6 +103,9 @@ int main()
 
   cout<<ans<<endl;
 }
+*/
+
+
 
 /*
 // version 3
@@ -141,5 +144,110 @@ int main()
   //for(int i=0;i<b.size();++i){cout<<b[i]<<" ";}
   //cout<<endl;
 
+}
+*/
+
+// version 4
+// dynamic programming
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<algorithm>
+#include<cmath>
+#define inf 0x7fffffff
+#define ll long long
+using namespace std;
+
+inline ll read()
+{
+    ll x=0,f=1;char ch=getchar();
+    while(ch>'9'||ch<'0'){if(ch=='-')f=-1;ch=getchar();}
+    while(ch>='0'&&ch<='9'){x=x*10+ch-'0';ch=getchar();}
+    return x*f;
+}
+
+int n,ans;
+int a[100005],f[100005][2][2];
+
+int main()
+{
+  n=read();
+  for(int i=1;i<=n;i++)
+    a[i]=read();
+  for(int i=1;i<=n;i++)
+    {
+      f[i][0][0]=f[i][1][1]=1;
+      if(i!=1)f[i][1][0]=2;
+      if(a[i]>a[i-1])
+        {
+          f[i][0][0]=max(f[i][0][0],f[i-1][0][0]+1);
+          f[i][1][0]=max(f[i][1][0],f[i-1][1][0]+1);
+          f[i][1][1]=max(f[i][1][1],f[i-1][0][0]+1);
+        }
+      else
+        f[i][1][1]=max(f[i][1][1],f[i-1][0][0]+1);
+      if(i!=1&&a[i]>a[i-2]+1)
+        f[i][1][0]=max(f[i][1][0],f[i-1][1][1]+1);
+      ans=max(ans,f[i][0][0]);
+      ans=max(ans,f[i][1][0]);
+      ans=max(ans,f[i][1][1]);
+    }
+  printf("%d",ans);
+}
+
+/*
+// version 5
+// two pointers
+#include<iostream>
+#include<cstdlib>
+#include<vector>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<queue>
+#include<set>
+#include<map>
+
+#define ms(x) memset(x,0,sizeof(x))
+#define For(a,b,c) for(int a = b ; a <= c ; a ++ )
+#define int64 long long
+#define real long double
+#define SZ size()
+
+int getint(){
+    char ch = getchar() ; int ret = 0 ; bool flag = true ;
+    while(( ch < '0' || ch > '9' ) && ch != '-' ) ch = getchar() ;
+    if( ch == '-' ) flag = false , ch = getchar() ;
+    while( ch >= '0' && ch <= '9' ) ret = ret * 10 + ch - '0' , ch = getchar() ;
+    return flag ? ret : - ret ;
+}
+
+using namespace std ;
+
+int a[100010] ;
+int n ;
+int g[100010] , l[100010] , r[100010] ;
+
+int main(){
+
+   cin >> n ;
+   for(int i = 1 ; i <= n ; i ++ ) a[i] = getint() ;
+   int fst = 0 ;
+   for(int i = 1; i <= n ; i ++ ){
+      fst = max(fst , i) ;
+      while(fst < n && a[fst + 1] > a[fst]) fst ++ ;
+      for(int j = i + 1 ; j <= fst + 1 ; j ++ ) l[j] = j - i ;
+      for(int j = i - 1 ; j <= fst - 1 ; j ++ ) r[j] = fst - j ;
+      i = fst ;
+   }
+
+   int ans = 1 ;
+   for(int i = 1 ; i <= n ; i ++ ) ans = max(ans , max(l[i] , r[i]) + 1) ;
+   for(int i = 1 ; i + 1 <= n ; i ++ )
+     if(a[i - 1] + 1 < a[i + 1])
+       ans = max(ans , l[i] + r[i] + 1) ;
+
+   cout << ans << endl ;
+   return 0 ;
 }
 */
