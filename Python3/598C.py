f7a0ec5b9bafa247633ea3ee839ff5a526e03059
Greenwicher <weizhiliu2009@gmail.com>
2016-02-11 20:59:31 +0800

    happy coding

--- Python3/598C.py
+++ Python3/598C.py
@@ -7,7 +7,7 @@ Created on Wed Feb 10 20:47:21 2016
 
 #%% Version 1
 from math import *
-# stores counterclockwise angle between (1,0) and each vector in a
+# stores counterclockwise angle between vector (1,0) and each vector in a
 a = []
 n = int(input())
 for _ in range(n):
@@ -33,4 +33,31 @@ else:
 print(i1+1,i2+1)
 
 #%% Version 2
-cosv = lambda a,b,c,d:(a*c+b*d)/((a*a+b*b)*(c*c+d*d))**0.5
+from math import *
+# stores counterclockwise angle between vector (1,0) and each vector in a
+a = []
+n = int(input())
+for i in range(n):
+    x,y = map(int,input().split())
+    # calculate counterclockwise angle between (1,0) and this vector
+    t = acos(x/sqrt(x**2+y**2))
+    a.append((i+1,[2*pi-t,t][y>=0],x,y))
+cmp = lambda x:x[1]
+a = sorted(a,key=cmp)
+# construct pairs for adjacent vectors
+b = []
+for i in range(n):
+    i1,i2 = a[i][0],a[(i+1)%n][0]
+    x1,y1 = a[i][2:]
+    x2,y2 = a[(i+1)%n][2:]
+    inner_prod = x1*x2 + y1*y2
+    inner_prod *= abs(inner_prod)
+    norm_prod = ((x1**2+y1**2)*(x2**2+y2**2))
+    b.append((i1,i2,inner_prod,norm_prod))
+# find the nearest vector
+better = lambda p1,p2: p1[2]*p2[3]>p2[2]*p1[3]
+ans = b[-1]
+for i in range(n):
+    if better(b[i],ans):
+        ans = b[i]
+print(ans[0],ans[1])
